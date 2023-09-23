3ca170ba716f1485c3d42754b2b7bc449ab7e7f3
Hideaki Terai <hideack@pepabo.com>
2023-09-23 15:59:14 +0900

    コマンドラインの解析はcli.js側で行う様にした

--- cli.js
+++ cli.js
@@ -1,2 +1,36 @@
-const main = require('./index');
+const { program } = require('commander');
+const { generatePivotTable } = require('./pivotTableGenerator');
+
+function parseOptions() {
+  program
+    .option('-i, --input <inputFile>', 'Input CSV file path', 'input.csv')
+    .option('-o, --output <outputFile>', 'Output CSV file path', 'output.csv')
+    .option('-r, --rowDimension <rowIndex>', 'Row dimension column index', 0)
+    .option('-c, --columnDimension <columnIndex>', 'Column dimension column index', 1)
+    .option('-v, --valueDimension <valueIndex>', 'Value dimension column index', 2)
+    .option('-e, --extraColumns <columnIndexes>', 'Additional column indexes to be included', '')
+    .option('--rowTotals', 'Include row totals in the output', false)
+    .option('--skipZeroTotals', 'Skip rows with zero totals in the output', false)
+    .option('--weeklyTotals', 'Include weekly totals in the output', false)
+    .option('--monthlyTotals', 'Include monthly totals in the output', false)
+    .option('--minRowTotal <minRowTotal>', 'Minimum row total for inclusion in the output', null)
+    .option('--maxRowTotal <maxRowTotal>', 'Maximum row total for inclusion in the output', null)
+    .option('--omitBody', 'Omit the body of the pivot table and only include totals', false)
+    .parse();
+
+  const options = program.opts();
+  options.rowDimension = parseInt(options.rowDimension);
+  options.columnDimension = parseInt(options.columnDimension);
+  options.valueDimension = parseInt(options.valueDimension);
+  options.minRowTotal = options.minRowTotal !== null ? parseFloat(options.minRowTotal) : null;
+  options.maxRowTotal = options.maxRowTotal !== null ? parseFloat(options.maxRowTotal) : null;
+  options.extraColumns = options.extraColumns.split(',').filter(x => x !== '').map(x => parseInt(x));
+  return options;
+}
+
+function main() {
+  const options = parseOptions();
+  generatePivotTable(options);
+}
+
 main();
