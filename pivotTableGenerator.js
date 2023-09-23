d226bef513a293b7f2844c25aaa6995c8817507e
Hideaki Terai <hideack@pepabo.com>
2023-09-23 15:59:46 +0900

    コマンドライン解析を削除してピボットテーブルを作る機能だけにした

--- pivotTableGenerator.js
+++ pivotTableGenerator.js
@@ -1,8 +1,5 @@
-module.exports = main;
-
 const fs = require('fs');
 const Papa = require('papaparse');
-const { program } = require('commander');
 
 class PivotTableGenerator {
   constructor(options) {
@@ -148,38 +145,7 @@ class PivotTableGenerator {
   }
 }
 
-function parseOptions() {
-  program
-    .option('-i, --input <inputFile>', 'Input CSV file path', 'input.csv')
-    .option('-o, --output <outputFile>', 'Output CSV file path', 'output.csv')
-    .option('-r, --rowDimension <rowIndex>', 'Row dimension column index', 0)
-    .option('-c, --columnDimension <columnIndex>', 'Column dimension column index', 1)
-    .option('-v, --valueDimension <valueIndex>', 'Value dimension column index', 2)
-    .option('-e, --extraColumns <columnIndexes>', 'Additional column indexes to be included', '')
-    .option('--rowTotals', 'Include row totals in the output', false)
-    .option('--skipZeroTotals', 'Skip rows with zero totals in the output', false)
-    .option('--weeklyTotals', 'Include weekly totals in the output', false)
-    .option('--monthlyTotals', 'Include monthly totals in the output', false)
-    .option('--minRowTotal <minRowTotal>', 'Minimum row total for inclusion in the output', null)
-    .option('--maxRowTotal <maxRowTotal>', 'Maximum row total for inclusion in the output', null)
-    .option('--omitBody', 'Omit the body of the pivot table and only include totals', false)
-    .parse();
-
-  const options = program.opts();
-  options.rowDimension = parseInt(options.rowDimension);
-  options.columnDimension = parseInt(options.columnDimension);
-  options.valueDimension = parseInt(options.valueDimension);
-  options.minRowTotal = options.minRowTotal !== null ? parseFloat(options.minRowTotal) : null;
-  options.maxRowTotal = options.maxRowTotal !== null ? parseFloat(options.maxRowTotal) : null;
-  options.extraColumns = options.extraColumns.split(',').filter(x => x !== '').map(x => parseInt(x));
-  return options;
-}
-
-async function main() {
-  const options = parseOptions();
-  console.log(`Input file: ${options.input}`);
-  console.log(`Output file: ${options.output}`);
-
+function generatePivotTable(options) {
   const fileContent = fs.readFileSync(options.input, 'utf8');
   const data = Papa.parse(fileContent, { header: false, skipEmptyLines: true }).data;
 
@@ -196,4 +162,6 @@ async function main() {
   console.log('Pivot table created successfully.');
 }
 
-module.exports = main;
+module.exports = {
+  generatePivotTable
+};
